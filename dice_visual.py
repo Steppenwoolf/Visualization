ab48f7a657520b0e844d8e243f5ce5b34c1aac6d
Lera <Lera@DESKTOP>
2020-05-05 14:20:56 +0300

    pop

--- dice_visual.py
+++ dice_visual.py
@@ -2,12 +2,15 @@ import pygal
 from die import Die
 
 # Создание 2x кубик
-die1 = Die()
-die2 = Die(10)
+die_a = 6
+die_b = 6
+times = 100000
+die1 = Die(die_a)
+die2 = Die(die_b)
 
 # Моделирование серии бросков с сохранением результатов в списке.
 results = []
-for roll_num in range(10000):
+for roll_num in range(times):
     result = die1.roll() + die2.roll()
     results.append(result)
 # Анализ результатов
@@ -19,19 +22,14 @@ for value in range(2, max_result+1):
 
 # Визуализация результатов
 hist = pygal.Bar()
-hist.title = "Results of rolling two D6 1000 times."
+hist.title = "Results of rolling two " + str(die_a) + ' & ' + str(die_b) + ' dices ' + str(times) + "times."
 hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
     '13', '14', '15', '16']
 hist.x_title = 'Result'
 hist.y_title = 'Frequency of result'
 
-hist.add('D6 + D10', frequencies)
+hist.add(str(die_a) + ' & ' + str(die_b), frequencies)
 hist.render_to_file('die_visual.svg')
 
 print(frequencies)
 
-# Анализ результатов.
-# frequencies = []
-# for value in range(1, die.num_sides+1):
-#     frequency = results.count(value)
-#     frequencies.append(frequency)
